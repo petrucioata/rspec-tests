5ba3e6d899559b82dd285bca209cd9ba155bfde3
Petru V. Cioata <petru.cioata@gmail.com>
2018-02-23 23:56:27 +0200

    Added new expectations for RaiseError matcher

--- Matchers/raise_error_matcher_spec.rb
+++ Matchers/raise_error_matcher_spec.rb
@@ -36,18 +36,32 @@ describe 'raise_error matcher' do
 
     context 'with match class' do
       context 'with string' do
-        it "matches the error message" do
+        it 'matches the error message' do
           expect { raise StandardError, 'this message exactly'}.
             to raise_error(StandardError, 'this message exactly')
         end
       end
 
       context 'with regular expression' do
-        it "matches the error message" do
+        it 'matches the error message' do
           expect { raise StandardError, "my message" }.
             to raise_error(StandardError, /my mess/)
         end
       end
+    end
+  end
+
+  context 'set expectation on error object passed to block' do
+    it 'raises NameError' do
+      expect { Object.new.foo }.to raise_error { |error|
+        expect(error).to be_a(NameError)
+      }
+    end
+  end
+
+  context 'expect no error at all' do
+    it 'does not raise' do
+      expect { Object.new.to_s }.not_to raise_error
     end
   end
 end
