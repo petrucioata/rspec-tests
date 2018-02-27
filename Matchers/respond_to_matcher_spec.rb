c339ea5e21468ca5e047219badabc173f8fcbc61
Petru V. Cioata <petru.cioata@gmail.com>
2018-02-27 23:58:22 +0200

    Respond to matcher - specify arguments

--- Matchers/respond_to_matcher_spec.rb
+++ Matchers/respond_to_matcher_spec.rb
@@ -18,4 +18,19 @@ describe 'respond_to matcher' do
     # it { expect(a_string).not_to respond_to(:sample, :size) }
     # it { expect(a_string).to respond_to(:sample, :size) }
   end
+
+  context 'when the arguments are specified' do
+    it { expect(777).to respond_to(:zero?).with(0).arguments }
+    it { expect(777).to respond_to(:between?).with(2).arguments }
+    it { expect(999).not_to respond_to(:upto).with(3).arguments }
+
+    it { expect("string").to respond_to(:size).with(0).arguments }
+    it { expect("string").to respond_to(:include?).with(1).arguments }
+    it { expect("test").not_to respond_to(:sample).with(10).arguments }
+    it { expect("test").not_to respond_to(:class).with(10).arguments }
+
+    it { expect(Hash.new).to respond_to(:keys).with(0).arguments }
+    it { expect(Hash.new).to respond_to(:has_value?).with(1).arguments }
+    it { expect(Hash.new).not_to respond_to(:key).with(0).arguments }
+  end
 end
