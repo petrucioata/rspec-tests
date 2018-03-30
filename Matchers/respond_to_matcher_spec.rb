8bef01f45cc04a8cf4c7419d58ca8dc494180cb9
Petru Cioata <petru.cioata@assist.ro>
2018-03-30 15:31:37 +0300

    Code improvements

--- Matchers/respond_to_matcher_spec.rb
+++ Matchers/respond_to_matcher_spec.rb
@@ -2,7 +2,6 @@ require 'rspec'
 require 'my_class'
 
 describe 'respond_to matcher' do
-  let(:my_class_object) { MyClass.new }
 
   context 'basic usage - a string and an array' do
     let(:a_string) { "just a test" }
@@ -37,55 +36,57 @@ describe 'respond_to matcher' do
     it { expect(Hash.new).not_to respond_to(:key).with(0).arguments }
   end
 
-  context 'when the argument range is specified' do
-    it { expect(my_class_object).to respond_to(:build).with(1..2).arguments }
-    it { expect(my_class_object).not_to respond_to(:build).with(0..1).arguments }
-    it { expect(my_class_object).not_to respond_to(:build).with(2..3).arguments }
-    it { expect(my_class_object).not_to respond_to(:build).with(0..3).arguments }
+  describe MyClass do
+    context 'when the argument range is specified' do
+      it { is_expected.to respond_to(:build).with(1..2).arguments }
+      it { is_expected.not_to respond_to(:build).with(0..1).arguments }
+      it { is_expected.not_to respond_to(:build).with(2..3).arguments }
+      it { is_expected.not_to respond_to(:build).with(0..3).arguments }
 
-    # deliberate failures
-    # it { is_expected.not_to respond_to(:build).with(1..2).arguments }
-    # it { is_expected.to respond_to(:build).with(0..1).arguments }
-    # it { is_expected.to respond_to(:build).with(2..3).arguments }
-    # it { is_expected.to respond_to(:build).with(0..3).arguments }
-  end
+      # deliberate failures
+      # it { is_expected.not_to respond_to(:build).with(1..2).arguments }
+      # it { is_expected.to respond_to(:build).with(0..1).arguments }
+      # it { is_expected.to respond_to(:build).with(2..3).arguments }
+      # it { is_expected.to respond_to(:build).with(0..3).arguments }
+    end
 
-  context 'when specify the unlimited arguments' do
-    it { expect(my_class_object).to respond_to(:greetings).with_unlimited_arguments }
-    it { expect(my_class_object).to respond_to(:greetings).with(1).argument.and_unlimited_arguments }
-    it { expect(my_class_object).not_to respond_to(:helooo).with_unlimited_arguments }
-    it { expect(my_class_object).not_to respond_to(:helooo).with(1).argument.and_unlimited_arguments }
+    context 'when specify the unlimited arguments' do
+      it { is_expected.to respond_to(:greetings).with_unlimited_arguments }
+      it { is_expected.to respond_to(:greetings).with(1).argument.and_unlimited_arguments }
+      it { is_expected.not_to respond_to(:helooo).with_unlimited_arguments }
+      it { is_expected.not_to respond_to(:helooo).with(1).argument.and_unlimited_arguments }
 
-    # deliberate failures
-    # it { expect(my_class_object).not_to respond_to(:greetings).with_unlimited_arguments }
-    # it { expect(my_class_object).not_to respond_to(:greetings).with(1).argument.and_unlimited_arguments }
-    # it { expect(my_class_object).to respond_to(:helooo).with_unlimited_arguments }
-    # it { expect(my_class_object).to respond_to(:helooo).with(1).argument.and_unlimited_arguments }
-  end
+      # deliberate failures
+      # it { is_expected.not_to respond_to(:greetings).with_unlimited_arguments }
+      # it { is_expected.not_to respond_to(:greetings).with(1).argument.and_unlimited_arguments }
+      # it { is_expected.to respond_to(:helooo).with_unlimited_arguments }
+      # it { is_expected.to respond_to(:helooo).with(1).argument.and_unlimited_arguments }
+    end
 
-  context 'when specify keywords' do
-    it { expect(my_class_object).to respond_to(:find).with_keywords(:limit, :offset) }
-    it { expect(my_class_object).to respond_to(:find).with_keywords(:limit) }
-    it { expect(my_class_object).to respond_to(:find).with(1).argument.and_keywords(:limit, :offset) }
-    it { expect(my_class_object).to respond_to(:find).with(1).argument.and_keywords(:offset) }
+    context 'when specify keywords' do
+      it { is_expected.to respond_to(:find).with_keywords(:limit, :offset) }
+      it { is_expected.to respond_to(:find).with_keywords(:limit) }
+      it { is_expected.to respond_to(:find).with(1).argument.and_keywords(:limit, :offset) }
+      it { is_expected.to respond_to(:find).with(1).argument.and_keywords(:offset) }
 
-    it { expect(my_class_object).not_to respond_to(:find).with_keywords(:limit, :extra_param) }
-    it { expect(my_class_object).not_to respond_to(:find).with(2).arguments.and_keywords(:offset) }
-    it { expect(my_class_object).not_to respond_to(:find).with(1).argument.and_keywords(:offset, :page) }
-  end
+      it { is_expected.not_to respond_to(:find).with_keywords(:limit, :extra_param) }
+      it { is_expected.not_to respond_to(:find).with(2).arguments.and_keywords(:offset) }
+      it { is_expected.not_to respond_to(:find).with(1).argument.and_keywords(:offset, :page) }
+    end
 
-  context 'when specify any keywords' do
-    it { expect(my_class_object).to respond_to(:any_keywords).with_any_keywords }
-    it { expect(my_class_object).to respond_to(:any_keywords).with_keywords(:name).and_any_keywords }
+    context 'when specify any keywords' do
+      it { is_expected.to respond_to(:any_keywords).with_any_keywords }
+      it { is_expected.to respond_to(:any_keywords).with_keywords(:name).and_any_keywords }
 
-    it { expect(my_class_object).not_to respond_to(:greetings).with_any_keywords }
-    it { expect(my_class_object).not_to respond_to(:greetings).with_keywords(:message).and_any_keywords }
-  end
+      it { is_expected.not_to respond_to(:greetings).with_any_keywords }
+      it { is_expected.not_to respond_to(:greetings).with_keywords(:message).and_any_keywords }
+    end
 
-  context 'when specify required keywords' do
-    it { expect(my_class_object).to respond_to(:required_keywords).with_keywords(:param) }
-    it { expect(my_class_object).to respond_to(:required_keywords).with_keywords(:param, :param_nil, :param_text) }
+    context 'when specify required keywords' do
+      it { is_expected.to respond_to(:required_keywords).with_keywords(:param) }
+      it { is_expected.to respond_to(:required_keywords).with_keywords(:param, :param_nil, :param_text) }
 
-    it { expect(my_class_object).not_to respond_to(:required_keywords).with_keywords(:param_text, :param_nil) }
+      it { is_expected.not_to respond_to(:required_keywords).with_keywords(:param_text, :param_nil) }
+    end
   end
 end
