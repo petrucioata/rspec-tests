f3567b87a09dc0382861126efbaee1f287da2f37
Petru Cioata <petru.cioata@assist.ro>
2018-04-30 15:00:37 +0300

    Yield_with_args matcher

--- matchers/yield_matchers_spec.rb
+++ matchers/yield_matchers_spec.rb
@@ -13,4 +13,22 @@ describe 'yield matchers' do
     it { expect { |arg| YieldClass.yield_twice_with(1, &arg) }.to yield_control.at_least(1) }
     it { expect { |arg| YieldClass.yield_twice_with(1, &arg) }.to yield_control.at_most(3).times }
   end
+
+  describe 'yield_with_args matcher' do
+    it { expect { |arg| YieldClass.yield_once_with('foo', &arg) }.to yield_with_args }
+    it { expect { |arg| YieldClass.yield_once_with('abc', &arg) }.to yield_with_args('abc') }
+    # it { expect { |arg| YieldClass.yield_once_with('abc', &arg) }.to yield_with_args('foo') } - F
+    it { expect { |arg| YieldClass.yield_once_with('xyz', &arg) }.to yield_with_args(String) }
+    it { expect { |arg| YieldClass.yield_once_with('bar', &arg) }.to yield_with_args(/ar/) }
+
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args('foo', 'bar') }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args(String, String) }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args(/fo/, /ar/) }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args(String, /ar/) }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 'bar', &arg) }.to yield_with_args('foo', /ar/) }
+
+    it { expect { |arg| YieldClass.yield_once_with('foo', 123, &arg) }.not_to yield_with_args(Integer, String) }
+    it { expect { |arg| YieldClass.yield_once_with('foo', 123, &arg) }.not_to yield_with_args('foo', /23/) }
+  end
 end
